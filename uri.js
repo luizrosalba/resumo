97748eb5035f29c83a8555edc025b44e1876d4dd
Luiz Rosalba <luizrosalba@gmail.com>
2020-12-13 09:07:20 -0300

    Update uri.js

--- uri.js
+++ uri.js
@@ -52,6 +52,50 @@ function validaOperador(ent)
    
 
 
+
+function SorteiaeValidaBimestres (alvo)
+{
+    let max = 1; 
+    let min = 0; 
+    saida = { 
+        mais: 0, 
+        menos:0,
+    }
+    let valida=false;
+    do {
+        sort = Math.random() * (max - min) + min;
+        rand= parseFloat(sort.toFixed(1))
+        saida.mais   = (parseFloat(alvo)+rand).toFixed(1);
+        saida.menos  = (parseFloat(alvo)-rand).toFixed(1);
+        (saida.menos>0 && saida.mais<=10)?valida=true:""; 
+    }while (valida!==true)
+    return saida; 
+}
+function SorteiaeValidaAtividades(alvo)
+{
+    let max = 1; 
+    let min = 0; 
+    saida = { 
+        mais: 0, 
+        menosmais:0,
+        menos2:0,
+    }
+    let valida=false;
+    do {
+        sort = Math.random() * (max - min) + min;
+        rand= parseFloat(sort.toFixed(1))
+        saida.mais    = parseFloat((parseFloat(alvo)+ rand).toFixed(1));
+        metadeRand = (rand/2); 
+        sort2 = Math.random() * (max - min) + min;
+        rand2 = parseFloat(sort2.toFixed(1))
+        saida.menos2      = parseFloat((parseFloat(alvo)- (metadeRand-rand2)).toFixed(1));
+        saida.menosmais   = parseFloat((parseFloat(alvo)- (metadeRand+rand2)).toFixed(1));
+        
+        (saida.menos2 > 0 &&  saida.menos2 <= 10 && saida.mais <= 10 && saida.menosmais<=10)?valida=true:""; 
+    }while (valida!==true)
+    return saida; 
+}
+
 //// funcao que deve ser executada no teste 
 //module.exports = function programa() {  /// chai e mocha 
 function programa() {   /// programa 
@@ -66,17 +110,98 @@ function programa() {   /// programa
 
     //let val = parseInt(g.next().value);
     //console.log(ent);
-    let entInt = parseInt(ent);
+    
+    /// gerar 4 numeros que a média vale alvo 
+    /// so funciona para num bim pares 
+    
+        let numBim = 4; 
+        var notasBim = {
+            idAluno: 0, 
+            alvo: 0.0,
+            notas: [],
+            notasAtividades: []
+        };
+        var atividades = {
+            idBim: 0, 
+            notas: []
+        }
+        let vecNotas = [];
 
-    let num_anos= entInt/365;
-    let nummeses= (entInt - (num_anos.toFixed(0)*365) )/30; 
-    let numdias = entInt - (num_anos.toFixed(0)*365) - (nummeses.toFixed(0)*30) ;
+        let numNotasPorBimestre = 3 ; 
+        let rand;
+        let max = 2; 
+        let min = 0; 
+        ent.forEach( (e,i) => {
+            //console.log(e);
+            notasBim.idAluno = i ; 
+            notasBim.alvo = e ; 
+            /// gerando os bimestres 
+            for (let index = 0; index < numBim; index=index+2) 
+            {
+                sort = Math.random() * (max - min) + min;
+                rand= parseFloat(sort.toFixed(1))
+                let saida = SorteiaeValidaBimestres(notasBim.alvo); 
+                notasBim.notas[index]   = saida.mais;
+                notasBim.notas[index+1] = saida.menos;
+            }
+            /// gerando as atividades 
+            /// para cada bimestre numNotasPorBimestre
+            for (let index = 0; index < numBim; index++)  /// para cada bimestre gera 3 notas 
+            {
+                    sort = Math.random() * (max - min) + min;
+                    rand= parseFloat(sort.toFixed(1))
+                    atividades.idBim = index; 
+                    let saida = SorteiaeValidaAtividades(notasBim.notas[index]); 
+                    atividades.notas.push(saida.mais);
+                    atividades.notas.push(saida.menos2);
+                    atividades.notas.push(saida.menosmais);
+                    notasBim.notasAtividades.push(atividades.notas);
+                    atividades = {
+                        idBim: 0, 
+                        notas: []
+                    }
+            }
+            // console.log(notasBim)
+            vecNotas.push(notasBim);
+            //}
+            notasBim = {
+                idAluno: 0, 
+                alvo: 0.0,
+                notas: [],
+                notasAtividades: []
+            };
+            console.log(i);
+        });
 
+        console.log(vecNotas[0]);
+
+        vecNotas.forEach( e => {
+            console.log(e.notasAtividades[0][0] + "," + "," +e.notasAtividades[0][1] + "," +"," + e.notasAtividades[0][2]+ "," +"," + e.notas[0]);
+        });
+        console.log(" ");
+        vecNotas.forEach( e => {
+            console.log(e.notasAtividades[1][0] + "," + "," +e.notasAtividades[1][1] + "," +"," + e.notasAtividades[1][2]+ "," +"," + e.notas[1]);
+        });
+        console.log(" ");
+        vecNotas.forEach( e => {
+            console.log(e.notasAtividades[2][0] + "," + "," +e.notasAtividades[2][1] + "," +"," + e.notasAtividades[2][2]+ "," +"," + e.notas[2]);
+        });
+        console.log(" ");
+        vecNotas.forEach( e => {
+            console.log(e.notasAtividades[3][0] + "," + "," +e.notasAtividades[3][1] + "," +"," + e.notasAtividades[3][2]+ "," +"," + e.notas[3]);
+        });
+        console.log(" ");
+
+
+        
+
+        
+        
+        
+
+       
    
-    console.log(num_anos.toFixed(0)+ " ano(s)");
-    console.log(nummeses.toFixed(0)+ " mes(es)");
-    console.log(numdias.toFixed(0)+ " dia(s)");
-
+    //console.log(entInt.toFixed(0));
     /// sera que o pribelam eh a funcao ? 
 
    // return saida;
